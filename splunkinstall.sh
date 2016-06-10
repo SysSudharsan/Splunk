02a8eadb8860090402111347e5b76116a9f3930b
SysSudharsan <SysSudharsan@users.noreply.github.com>
2016-06-10 19:20:49 +0530

    Create splunkinstall.sh

---
+++ splunkinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -y update
+
+sudo apt-get -y install wget
+
+sudo wget https://sysgainelkstorage.blob.core.windows.net/splunk-install/splunk-6.4.1-debde650d26e-linux-2.6-amd64.deb
+
+sudo dpkg -i splunk-6.4.1-debde650d26e-linux-2.6-amd64.deb
+
+sudo /opt/splunk/bin/splunk start --accept-license
